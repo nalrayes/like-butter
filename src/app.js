d8ed0fb257a9bd355b6a678fd2525f0eaa420159
Naif Alrayes <thenalrayes@gmail.com>
2017-11-07 16:52:53 -0500

    feedback page that successfully send me emails as feedback is fully functional

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const sgMail = require('@sendgrid/mail');
+const config = require('../config.json');
 
 const app = express();
 
@@ -10,31 +12,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 // to see how that would work with bootsrap/react
 
 // home endpoint
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
+//
+// });
 
+app.get('/feedback', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/feedback.html'));
 });
 
-app.get('/albums', (req, res) => {
-
+app.post('/feedback', (req, res) => {
+  sgMail.setApiKey(config.sendGridKey);
+  const msg = {
+    to: 'thenalrayes@gmail.com',
+    from: 'thenalrayes@gmail.com',
+    subject: req.body.senderName,
+    text: req.body.emailBody,
+  };
+  sgMail.send(msg);
+  res.redirect('/feedback');
 });
-
-app.get('/photo', (req, res) => {
-  // needs photo id to work
-  // if no query submitted, give 404
-  // if photo_id is invalid, give 404
-  // else show the picture
-});
-
-app.get ('/about', (req, res) => {
-
-});
-
-app.get('/album', (req, res) => {
-  // needs album_id to work
-  // if no query submitted, give 404
-  // if album_id is invalid, give 404
-  // else show the album
-});
+//
+// app.get('/photo', (req, res) => {
+//   // needs photo id to work
+//   // if no query submitted, give 404
+//   // if photo_id is invalid, give 404
+//   // else show the picture
+// });
+//
+// app.get ('/about', (req, res) => {
+//
+// });
+//
+// app.get('/album', (req, res) => {
+//   // needs album_id to work
+//   // if no query submitted, give 404
+//   // if album_id is invalid, give 404
+//   // else show the album
+// });
 
 
 
