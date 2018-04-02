f0f68ed8fa0a759d78cee4ab509b844bfff8d750
nalrayes <thenalrayes@gmail.com>
2018-04-02 00:27:49 -0400

    cleaned some shit up

--- frontend/src/react-images/src/ImageDetails.js
+++ frontend/src/react-images/src/ImageDetails.js
@@ -39,32 +39,32 @@ class ImageDetails extends Component {
     // )
     return(
       <div className={css(this.classes.gapContainer)}>
-      <div className={css(this.classes.detailsContainer)}>
-      <div className={css(this.classes.details)}>
-      <p className={css(this.classes.titleParagraph)}>
-      <span className={css(this.classes.title)}>
-        {imageDetails[currentImage].title}
-      </span>
-      <span className={css(this.classes.date)}>, June 2017</span>
-      <br />
-      <span className={css(this.classes.location)}> {imageDetails[currentImage].location} </span>
-      </p>
-      <p className={css(this.classes.descriptionParagraph)}>
-      I randomly took this picture while in the car with my pal Moni while she was showing me around San Fransisco. This kind of happened by accident but turned out great, I love the way the light sort of fades out the powerlines and creates a cool shadow over the church. I was listening Madvilliany for the millionth time here, and Rainbows is a strange singy interlude that takes you into my favorite track in the album. <br />
-      </p>
-      <br />
-      <p className={css(this.classes.footerParagraph)}>
-      Taken at 1.0f, 1/60s, 350 ISO.
-      <br />
-      {takenOn}
-      </p>
-      </div>
-      </div>
-      <div className={css(this.classes.detailsContainerWithGap)}>
-      <div className={css(this.classes.centerMe)}>
-      <iframe title='Song' src={imageDetails[currentImage].spotifyURL} width="100%" height="80" frameborder="0" allowtransparency="true"></iframe>
-      </div>
-      </div>
+        <div className={css(this.classes.detailsContainer)}>
+          <div className={css(this.classes.details)}>
+            <p className={css(this.classes.titleParagraph)}>
+              <span className={css(this.classes.title)}>
+                {imageDetails[currentImage].title}
+              </span>
+              <span className={css(this.classes.date)}>, June 2017</span>
+              <br />
+              <span className={css(this.classes.location)}> {imageDetails[currentImage].location} </span>
+            </p>
+            <p className={css(this.classes.descriptionParagraph)}>
+              I randomly took this picture while in the car with my pal Moni while she was showing me around San Fransisco. This kind of happened by accident but turned out great, I love the way the light sort of fades out the powerlines and creates a cool shadow over the church. I was listening Madvilliany for the millionth time here, and Rainbows is a strange singy interlude that takes you into my favorite track in the album. <br />
+            </p>
+            <br />
+            <p className={css(this.classes.footerParagraph)}>
+              Taken at 1.0f, 1/60s, 350 ISO.
+            <br />
+            {takenOn}
+            </p>
+          </div>
+        </div>
+        <div className={css(this.classes.detailsContainerWithGap)}>
+          <div className={css(this.classes.centerMe)}>
+            <iframe title='Song' src={imageDetails[currentImage].spotifyURL} width="100%" height="80" frameborder="0" allowtransparency="true"></iframe>
+          </div>
+        </div>
       </div>
     )
   }
